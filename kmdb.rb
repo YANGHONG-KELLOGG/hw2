ff98d7604eb97a1d3f92ffb479115a34c831f139
YANGHONG-KELLOGG <yang.hong@kellogg.northwestern.edu>
2023-02-06 04:34:50 +0000

    Final result

--- kmdb.rb
+++ kmdb.rb
@@ -84,21 +84,21 @@ WB = Studio.find_by({"name"=>"Warner Bros."})
 
 movie = Movie.new
 movie["title"] = "Batman Begins"
-movie["year_released"] = "2005"
+movie["year_released"] = 2005
 movie["rated"] = "PG-13"
 movie["sutdio_id"] = WB["id"]
 movie.save
 
 movie = Movie.new
 movie["title"] = "The Dark Knight"
-movie["year_released"] = "2008"
+movie["year_released"] = 2008
 movie["rated"] = "PG-13"
 movie["sutdio_id"] = WB["id"]
 movie.save
 
 movie = Movie.new
 movie["title"] = "The Dark Knight Rises"
-movie["year_released"] = "2012"
+movie["year_released"] = 2012
 movie["rated"] = "PG-13"
 movie["sutdio_id"] = WB["id"]
 movie.save
